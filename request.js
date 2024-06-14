1bb54233815f720661f6eb585fc1aebb8ccf5554
Kian731 <140600174+Kian731@users.noreply.github.com>
2024-06-14 08:24:39 +0000

    change

---
+++ request.js
@@ -0,0 +1,12 @@
+
+var changetext = document.getElementById("change");
+
+document.getElementById("button").onclick = function () {
+    if (changetext.dataset.num == "2") {
+        changetext.dataset.num = "1"
+        changetext.innerHTML = "start123";
+    } else {
+        changetext.dataset.num = "2";
+        changetext.innerHTML = "4567end";
+    }
+}
